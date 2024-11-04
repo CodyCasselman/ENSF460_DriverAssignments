9400c701a4747ea0f777dd98af152796ab59a378
keeryn04 <keerynjohnson@gmail.com>
2024-11-04 15:42:11 -0700

    Initial Project 2 Commit Again

---
+++ ENSF460Project2/Project 2.X/clkChange.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   clkChange.h
+ * Authors: Cody C, Evan M, Keeryn J
+ */
+
+#ifndef CHANGECLK_H
+#define CHANGECLK_H
+
+#include <xc.h>
+
+// Function declarations
+void newClk(unsigned int clkval);
+
+#endif // CHANGECLK_H
